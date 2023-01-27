0987758735f01791f6a0c745f60136238ee119ae
omjinLTS <omjin7g@gmail.com>
2023-01-27 14:54:22 +0900

    first commit

---
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,22 @@
+package hello.hellospring.domain;
+
+public class Member {
+    private Long id;
+    private String Name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
